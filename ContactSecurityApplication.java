28d4412dcca1254d3e6b51714a9ae4b08c57658b
ratan-singh03 <ratanparsola@gmail.com>
2026-08-20 07:43:54 +0100

    Create ContactSecurityApplication.java

---
+++ ContactSecurityApplication.java
@@ -0,0 +1,12 @@
+package com.example.contactsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContactSecurityApplication.class, args);
+    }
+}
